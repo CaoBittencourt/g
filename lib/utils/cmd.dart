d467dd5afb0b75de8c2f2c144e74b94d22d7da4f
CaoBittencourt <caobittencourt@gmail.com>
2025-09-01 18:58:11 -0300

    gmm debug

--- lib/utils/cmd.dart
+++ lib/utils/cmd.dart
@@ -1,6 +1,7 @@
 import 'dart:io' show Process;
 
 Future<Process> cmd(List<String> commands) async {
-  print("command: ${commands.join(" && ")}\n");
+  print("command: ${commands.join(" && ")}");
+  print("newline\n");
   return await Process.start("sh", ["-c", commands.join(" && ")]);
 }
