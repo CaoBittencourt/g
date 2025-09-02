fa4514e1c638a2170c26b20a03f676bde1f19e6c
CaoBittencourt <caobittencourt@gmail.com>
2025-09-01 21:56:52 -0300

    cmd newline

--- lib/utils/cmd.dart
+++ lib/utils/cmd.dart
@@ -1,5 +1,5 @@
 import 'dart:io' show Process;
 
 Future<Process> cmd(List<String> commands) async {
-  return await Process.start("sh", ["-c", commands.join(" && ")]);
+  return await Process.start("sh", ["-c", commands.join(" && echo \\n && ")]);
 }
